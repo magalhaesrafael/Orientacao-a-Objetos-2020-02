e6ab3503ce14064e3bb6a2ede74bea31f5daba27
rafa_ <rafa_@DESKTOP-0OBJLR3>
2021-02-01 20:04:45 -0300

    Teste commit.

---
+++ Teste/src/primeiro/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package primeiro;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Primeiro programa em Java.");
+	}
+
+}
